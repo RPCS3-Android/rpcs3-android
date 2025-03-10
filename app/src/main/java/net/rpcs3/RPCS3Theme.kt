5827138887cdf52a97a07bf54fe5bb5824700c5a
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-03-10 17:25:44 +0530

    Update RPCS3Theme.kt

--- app/src/main/java/net/rpcs3/RPCS3Theme.kt
+++ app/src/main/java/net/rpcs3/RPCS3Theme.kt
@@ -4,6 +4,12 @@ package net.rpcs3
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.isSystemInDarkTheme
+import android.app.Activity
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 @Composable
 fun RPCS3Theme(
@@ -11,6 +17,20 @@ fun RPCS3Theme(
     content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) darkColorScheme() else lightColorScheme()
+
+    val view = LocalView.current
+    val activity = view.context as? Activity
+
+    SideEffect {
+        activity?.window?.apply {
+            statusBarColor = Color.TRANSPARENT
+            navigationBarColor = Color.TRANSPARENT
+            isNavigationBarContrastEnforced = false
+            val insetsController = WindowInsetsControllerCompat(this, decorView)
+            insetsController.isAppearanceLightNavigationBars = !darkTheme
+            insetsController.isAppearanceLightStatusBars = !darkTheme
+        }
+    }
 
     MaterialTheme(
         colorScheme = colors,
