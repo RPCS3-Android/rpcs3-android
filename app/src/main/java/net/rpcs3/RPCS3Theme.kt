93214c58f65e855ba5973d891590ddafa694e6fa
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-03-10 15:04:31 +0530

    Create RPCS3Theme.kt

---
+++ app/src/main/java/net/rpcs3/RPCS3Theme.kt
@@ -0,0 +1,19 @@
+
+package net.rpcs3
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RPCS3Theme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) darkColorScheme() else lightColorScheme()
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        content = content
+    )
+}
