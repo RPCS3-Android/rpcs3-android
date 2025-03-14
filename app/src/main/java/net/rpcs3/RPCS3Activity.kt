988454e1bb1bffe871dd44ec5b4cddbd3593e614
Muhammad Ashhal <96419703+MAshhal@users.noreply.github.com>
2025-03-15 00:42:01 +0500

    feat(RPCS3Activity): apply system bar insets to PadOverlay.

--- app/src/main/java/net/rpcs3/RPCS3Activity.kt
+++ app/src/main/java/net/rpcs3/RPCS3Activity.kt
@@ -2,9 +2,13 @@ package net.rpcs3
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.ViewGroup.MarginLayoutParams
+import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.view.updateLayoutParams
+import net.rpcs3.overlay.PadOverlay
 
 class RPCS3Activity : Activity() {
     private lateinit var unregisterUsbEventListener: () -> Unit
@@ -32,6 +36,23 @@ class RPCS3Activity : Activity() {
             // System Bars include status, navigation and caption bars.
             hide(WindowInsetsCompat.Type.systemBars())
             systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+        applyInsetsToPadOverlay()
+    }
+
+    private fun applyInsetsToPadOverlay() {
+        val padOverlay = findViewById<PadOverlay>(R.id.padOverlay)
+        ViewCompat.setOnApplyWindowInsetsListener(padOverlay) { view, windowInsets ->
+            // I don't think we need `displayCutout` insets here as well
+            // Since there is hardly any overlay overlapping with it
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updateLayoutParams<MarginLayoutParams> {
+                leftMargin = insets.left
+                rightMargin = insets.right
+                topMargin = insets.top
+                bottomMargin = insets.bottom
+            }
+            WindowInsetsCompat.CONSUMED
         }
     }
 
