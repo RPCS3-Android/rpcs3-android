5d96523ba2524ca1516fd5926c8ef0f0c13fc80d
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-03-14 19:10:34 +0530

    FileUtil: fix subFolders

--- app/src/main/java/net/rpcs3/utils/FileUtil.kt
+++ app/src/main/java/net/rpcs3/utils/FileUtil.kt
@@ -3,31 +3,22 @@ package net.rpcs3.utils
 import android.content.Context
 import android.content.SharedPreferences
 import android.net.Uri
-import android.provider.DocumentsContract
+import androidx.documentfile.provider.DocumentFile
 import android.util.Log
 import net.rpcs3.PrecompilerService
 import net.rpcs3.PrecompilerServiceAction
 
 object FileUtil {
-    fun installPackages(context: Context, folderUri: Uri) {
-        val contentResolver = context.contentResolver
-        val childrenUri = DocumentsContract.buildChildDocumentsUriUsingTree(folderUri, DocumentsContract.getTreeDocumentId(folderUri))
-
-        contentResolver.query(childrenUri, 
-            arrayOf(DocumentsContract.Document.COLUMN_DOCUMENT_ID, DocumentsContract.Document.COLUMN_MIME_TYPE), 
-            null, null, null)?.use { cursor ->
+    fun installPackages(context: Context, folderUri: Uri, depth: Int = 2) {
+        if (depth <= 0) return
+        val rootFolder = DocumentFile.fromTreeUri(context, folderUri) ?: return
         
-            while (cursor.moveToNext()) {
-                val documentId = cursor.getString(0)
-                val mimeType = cursor.getString(1)
-                val fileUri = DocumentsContract.buildDocumentUriUsingTree(folderUri, documentId)
-
-                if (mimeType == DocumentsContract.Document.MIME_TYPE_DIR) {
-                    installPackages(context, fileUri)
-                } else {
-                    Log.d("Install", "Installing package: $fileUri")
-                    PrecompilerService.start(context, PrecompilerServiceAction.Install, fileUri)
-                }
+        for (file in rootFolder.listFiles()) {
+            if (!file.isDirectory) {
+                Log.d("Install", "Installing package: ${file.uri}")
+                PrecompilerService.start(context, PrecompilerServiceAction.Install, file?.uri!!)
+            } else {
+                installPackages(context, file?.uri!!, depth - 1)
             }
         }
     }
