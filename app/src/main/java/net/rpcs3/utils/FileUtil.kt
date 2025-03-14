233025a672aeb567d0da354f8e3e9473417e0c3c
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-03-14 17:50:42 +0530

    FileUtil: installPackages: fix crashes + also search in sub folders if exists

--- app/src/main/java/net/rpcs3/utils/FileUtil.kt
+++ app/src/main/java/net/rpcs3/utils/FileUtil.kt
@@ -13,11 +13,18 @@ object FileUtil {
         val contentResolver = context.contentResolver
         val childrenUri = DocumentsContract.buildChildDocumentsUriUsingTree(folderUri, DocumentsContract.getTreeDocumentId(folderUri))
 
-        contentResolver.query(childrenUri, arrayOf(DocumentsContract.Document.COLUMN_DOCUMENT_ID), null, null, null)?.use { cursor ->
+        contentResolver.query(childrenUri, 
+            arrayOf(DocumentsContract.Document.COLUMN_DOCUMENT_ID, DocumentsContract.Document.COLUMN_MIME_TYPE), 
+            null, null, null)?.use { cursor ->
+        
             while (cursor.moveToNext()) {
                 val documentId = cursor.getString(0)
+                val mimeType = cursor.getString(1)
                 val fileUri = DocumentsContract.buildDocumentUriUsingTree(folderUri, documentId)
-                if (cursor.getString(1) != DocumentsContract.Document.MIME_TYPE_DIR) {
+
+                if (mimeType == DocumentsContract.Document.MIME_TYPE_DIR) {
+                    installPackages(context, fileUri)
+                } else {
                     Log.d("Install", "Installing package: $fileUri")
                     PrecompilerService.start(context, PrecompilerServiceAction.Install, fileUri)
                 }
