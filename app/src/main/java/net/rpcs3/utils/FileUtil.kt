4f1ccad1c1e01a91f1c2bbf330138af3cbf89d37
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-03-16 12:28:43 +0530

    Update FileUtil.kt

--- app/src/main/java/net/rpcs3/utils/FileUtil.kt
+++ app/src/main/java/net/rpcs3/utils/FileUtil.kt
@@ -3,6 +3,7 @@ package net.rpcs3.utils
 import android.content.Context
 import android.content.SharedPreferences
 import android.net.Uri
+import android.provider.DocumentsContract
 import androidx.documentfile.provider.DocumentFile
 import android.util.Log
 import net.rpcs3.PrecompilerService
@@ -15,9 +16,8 @@ object FileUtil {
 
         while (workList.isNotEmpty()) {
             val currentFolderUri = workList.removeFirst()
-            val currentFolder = DocumentFile.fromTreeUri(context, currentFolderUri) ?: continue
 
-            currentFolder.listFiles().forEach { item ->
+            listFiles(currentFolderUri).forEach { item ->
                 if (item.isDirectory) {
                     workList.add(item.uri)
                 } else if (item.isFile) {
@@ -31,4 +31,47 @@ object FileUtil {
     fun saveGameFolderUri(prefs: SharedPreferences, uri: Uri) {
         prefs.edit().putString("selected_game_folder", uri.toString()).apply()
     }
+
+    fun listFiles(uri: Uri): Array<SimpleDocument> {
+        val columns = arrayOf(
+            DocumentsContract.Document.COLUMN_DOCUMENT_ID,
+            DocumentsContract.Document.COLUMN_DISPLAY_NAME,
+            DocumentsContract.Document.COLUMN_MIME_TYPE
+        )
+        var c: Cursor? = null
+        val results: MutableList<SimpleDocument> = ArrayList()
+        try {
+            val docId = if (isRootTreeUri(uri)) {
+                DocumentsContract.getTreeDocumentId(uri)
+            } else {
+                DocumentsContract.getDocumentId(uri)
+            }
+
+            val childrenUri = DocumentsContract.buildChildDocumentsUriUsingTree(uri, docId)
+            c = context.contentResolver.query(childrenUri, columns, null, null, null)
+            while (c!!.moveToNext()) {
+                val documentId = c.getString(0)
+                val documentName = c.getString(1)
+                val documentMimeType = c.getString(2)
+                val documentUri = DocumentsContract.buildDocumentUriUsingTree(uri, documentId)
+                val document = SimpleDocument(documentName, documentMimeType, documentUri)
+                results.add(document)
+            }
+        } catch (e: Exception) {
+            Log.e("FileUtil", "Cannot list file error: " + e.message)
+        } finally {
+            c.close()
+        }
+        return results.toTypedArray<SimpleDocument>()
+    }
+
+    fun isRootTreeUri(uri: Uri): Boolean {
+        val paths = uri.pathSegments
+        return paths.size == 2 && "tree" == paths[0]
+    }
+}
+
+class SimpleDocument(val filename: String, val mimeType: String, val uri: Uri) {
+    val isDirectory: Boolean
+        get() = mimeType == DocumentsContract.Document.MIME_TYPE_DIR
 }
