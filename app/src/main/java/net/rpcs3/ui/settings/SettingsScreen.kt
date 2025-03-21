b3998940d59d8ccc4fd748d5a12d2d53bbe35c48
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-03-21 15:19:58 +0530

    SettingsScreen: fix enum value resetting error

--- app/src/main/java/net/rpcs3/ui/settings/SettingsScreen.kt
+++ app/src/main/java/net/rpcs3/ui/settings/SettingsScreen.kt
@@ -171,7 +171,7 @@ fun AdvancedSettingsScreen(
                                             message = "Do you want to reset '$key' to its default value?",
                                             onConfirm = {
                                                 if (RPCS3.instance.settingsSet(itemPath, def.toString())) {
-                                                    itemObject.put("value", def)
+                                                    itemObject.put("value", "\"" + def + "\"")
                                                     itemValue = def
                                                 } else {
                                                     AlertDialogQueue.showDialog("Setting error", "Failed to reset $key")
